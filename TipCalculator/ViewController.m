ffa438c4790cd6bda60a1221b3e92573e6623554
Kevin T <kevin.tai.mail@gmail.com>
2018-02-25 18:39:40 -0800

    split amount implemented

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -8,15 +8,41 @@
 
 #import "ViewController.h"
 
+float floatTipAmount;
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeople;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
+@property (weak, nonatomic) IBOutlet UITextField *splitAmount;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)sliderValueChanged:(id)sender {
+  self.numberOfPeople.text = [NSString stringWithFormat:@"%d", (int) self.numberOfPeopleSlider.value];
+}
+
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  self.tipAmountLabel.text = @"0";
+  self.tipPercentageTextField.text = @"0";
+  self.numberOfPeople.text = @"2";
+}
+- (IBAction)calculateSplitAmount:(id)sender {
+  
+  
+  NSString *stringBillAmount = [self.billAmountTextField text];
+  float floatBillAmount = [stringBillAmount floatValue];
+
+  float floatNumberOfPeople = (int) self.numberOfPeopleSlider.value;
+  
+  floatTipAmount = [self.tipAmountLabel.text floatValue];
+  
+  float floatSplitAmount = ( floatBillAmount + floatTipAmount )/ floatNumberOfPeople;
+  
+  self.splitAmount.text = [NSString stringWithFormat:@"%.2f", floatSplitAmount];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -37,10 +63,11 @@
   
   NSString *stringBillAmount = [self.billAmountTextField text];
   float floatBillAmount = [stringBillAmount floatValue];
+  
   NSString *stringTipPercentage = [self.tipPercentageTextField text];
   float floatTipPercentage = [stringTipPercentage floatValue];
   
-  float floatTipAmount = floatBillAmount * floatTipPercentage / 100;
+  floatTipAmount = floatBillAmount * floatTipPercentage / 100;
   
   NSString *stringTipAmount = [NSString stringWithFormat:@"%.2f", floatTipAmount];
 //  NSLog(@"%@", stringTipAmount);
